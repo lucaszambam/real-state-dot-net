f253fd349659d0d15cbbe855c3ea245ee695f1e4
Lucas Zambam <lucaszambam7896@gmail.com>
2024-03-08 21:36:43 -0300

    add annotations

--- real-state-dot-net/Models/State.cs
+++ real-state-dot-net/Models/State.cs
@@ -1,9 +1,15 @@
-﻿namespace RealStateDotNet.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RealStateDotNet.Models
 {
     public class State
     {
+
+        [Key]
+        [StringLength(2, MinimumLength = 2, ErrorMessage = "2 characters required to State Acronym.")]
         public string Acronym { get; set; }
-        
+
+        [Required(ErrorMessage = "is required.")]
         public string Name { get; set; }
     }
 }
