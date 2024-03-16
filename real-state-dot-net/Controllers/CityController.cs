ac16b2c544cd181e17c91405cc7a6dcc82064aac
Lucas Zambam <lucaszambam7896@gmail.com>
2024-03-15 21:30:13 -0300

    add city controller

---
+++ real-state-dot-net/Controllers/CityController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RealStateDotNet.Data;
+using RealStateDotNet.Models;
+
+namespace RealStateDotNet.Controllers
+{
+    public class CityController : Controller
+    {
+        private readonly RealStateDotNetContext _context;
+
+        public CityController(RealStateDotNetContext context)
+        {
+            _context = context;
+        }
+
+        // GET: City
+        public async Task<IActionResult> Index()
+        {
+            var realStateDotNetContext = _context.City.Include(c => c.State);
+            return View(await realStateDotNetContext.ToListAsync());
+        }
+
+        // GET: City/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var city = await _context.City
+                .Include(c => c.State)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            return View(city);
+        }
+
+        // GET: City/Create
+        public IActionResult Create()
+        {
+            ViewData["StateAcronym"] = new SelectList(_context.State, "Acronym", "Acronym");
+            return View();
+        }
+
+        // POST: City/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,StateAcronym")] City city)
+        {
+            if (ModelState.IsValid)
+            {
+                city.Id = Guid.NewGuid();
+                _context.Add(city);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["StateAcronym"] = new SelectList(_context.State, "Acronym", "Acronym", city.StateAcronym);
+            return View(city);
+        }
+
+        // GET: City/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var city = await _context.City.FindAsync(id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+            ViewData["StateAcronym"] = new SelectList(_context.State, "Acronym", "Acronym", city.StateAcronym);
+            return View(city);
+        }
+
+        // POST: City/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Name,StateAcronym")] City city)
+        {
+            if (id != city.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(city);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CityExists(city.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["StateAcronym"] = new SelectList(_context.State, "Acronym", "Acronym", city.StateAcronym);
+            return View(city);
+        }
+
+        // GET: City/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var city = await _context.City
+                .Include(c => c.State)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            return View(city);
+        }
+
+        // POST: City/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var city = await _context.City.FindAsync(id);
+            if (city != null)
+            {
+                _context.City.Remove(city);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CityExists(Guid id)
+        {
+            return _context.City.Any(e => e.Id == id);
+        }
+    }
+}
