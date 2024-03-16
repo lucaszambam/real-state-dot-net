b41e7e56ce1d4af4e476c8b76d86591021c96c70
Lucas Zambam <lucaszambam7896@gmail.com>
2024-03-15 21:31:53 -0300

    add city migrations

--- real-state-dot-net/Migrations/20240316001846_City.Designer.cs
+++ real-state-dot-net/Migrations/20240316001846_City.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using RealStateDotNet.Data;
@@ -11,9 +12,11 @@ using RealStateDotNet.Data;
 namespace RealStateDotNet.Migrations
 {
     [DbContext(typeof(RealStateDotNetContext))]
-    partial class RealStateDotNetContextModelSnapshot : ModelSnapshot
+    [Migration("20240316001846_City")]
+    partial class City
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ real-state-dot-net/Migrations/20240316001846_City.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealStateDotNet.Migrations
+{
+    /// <inheritdoc />
+    public partial class City : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "City",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    StateAcronym = table.Column<string>(type: "character varying(2)", maxLength: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_City", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_City_State_StateAcronym",
+                        column: x => x.StateAcronym,
+                        principalTable: "State",
+                        principalColumn: "Acronym",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_City_StateAcronym",
+                table: "City",
+                column: "StateAcronym");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "City");
+        }
+    }
+}
