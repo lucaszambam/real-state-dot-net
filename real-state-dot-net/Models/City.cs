5398ba6abdfd73ad3e7b85ab580f4849f2f439a7
Lucas Zambam <lucaszambam7896@gmail.com>
2024-03-15 21:32:33 -0300

    add city model

--- real-state-dot-net/Models/City.cs
+++ real-state-dot-net/Models/City.cs
@@ -1,9 +1,18 @@
-﻿namespace RealStateDotNet.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RealStateDotNet.Models
 {
     public class City
     {
-
+        [Key]
         public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "is required.")]
         public string Name { get; set; }
+
+        [StringLength(2, MinimumLength = 2, ErrorMessage = "2 characters required to State Acronym.")]
+        public string StateAcronym { get; set; }
+
+        public State State {  get; set; }
     }
 }
